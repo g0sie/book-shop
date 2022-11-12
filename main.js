fecac218b158fd036c85c70ecc21add984084d5b
g0sie <malgorzataderkacz3@gmail.com>
2022-11-12 18:45:40 +0100

    feat: add base layout

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 "use strict";
 
+const container = document.createElement("div");
+container.className = "container";
+document.body.appendChild(container);
+
 const h1 = document.createElement("h1");
 h1.append("book shop");
-document.body.appendChild(h1);
+container.appendChild(h1);
+
+const main = document.createElement("main");
+container.appendChild(main);
+
+const bookCatalog = document.createElement("section");
+bookCatalog.className = "book-catalog";
+main.appendChild(bookCatalog);
+
+const orderBook = document.createElement("section");
+orderBook.className = "order-book";
+main.appendChild(orderBook);
